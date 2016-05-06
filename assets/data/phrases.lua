5b1f64ff8007a3900a3d85dc148f097ed1530ad0
Marco Lizza <marco.lizza@gmail.com>
2016-05-06 17:56:54 +0200

    Fixing phrases.

--- assets/data/phrases.lua
+++ assets/data/phrases.lua
@@ -1,109 +1,3 @@
-“All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a garden, and she plucked another flower and ran with it to her mother. I suppose she must have looked rather delightful, for Mrs Darling put her hand to her heart and cried, ‘Oh, why can’t you remain like this for ever!’ This was all that passed between them on the subject, but henceforth Wendy knew that she must grow up. You always know after you are two. Two is the beginning of the end.” 
-― J.M. Barrie, Peter Pan
-
-“You don't stop laughing when you grow old, you grow old when you stop laughing.” 
-― George Bernard Shaw
-
-“Men do not quit playing because they grow old; they grow old because they quit playing.” 
-― Oliver Wendell Holmes Sr.
-
-“Wisdom comes with winters” 
-― Oscar Wilde
-
-“The really frightening thing about middle age is that you know you'll grow out of it.” 
-― Doris Day
-
-“Everyday is one less day.” 
-― Tom Ford
-
-“I reckon responsible behavior is something to get when you grow older. Like varicose veins.” 
-― Terry Pratchett, Wyrd Sisters
-
-“I should like to ask you: -- Does your childhood seem far off? Do the days when you sat at your mother's knee, seem days of very long ago?" Responding to his softened manner, Mr. Lorry answered: "Twenty years back, yes; at this time of my life, no. For, as I draw closer and closer to the end, I travel in the circle, nearer and nearer to the beginning. It seems to be one of the kind smoothings and preparings of the way. My heart is touched now, by many remembrances that had long fallen asleep, of my pretty young mother (and I so old!), and by many associations of the days when what we call the World was not so real with me, and my faults were not confirmed with me.” 
-― Charles Dickens, A Tale of Two Cities
-
-“[LADY BRACKNELL]
-Thirty-five is a very attractive age. London society is full of women of the very highest birth who have, of their own free choice, remained thirty-five for years. Lady Dumbleton is an instance in point. To my own knowledge she has been thirty-five ever since she arrived at the age of forty, which was many years ago now.” 
-― Oscar Wilde, The Importance of Being Earnest
-
-“The older I get, the better I was.” 
-― Van Dyke Parks
-
-“One is alone when the last one who remembers is gone. I have nephews and nieces and kind friends---but there's no one who knew me as a young girl---non one who belongs to the old days. I've been alone for quite a long time now.” 
-― Agatha Christie
-
-“Let us cherish and love old age; for it is full of pleasure if one knows how to use it. Fruits are most welcome when almost over; youth is most charming at its close; the last drink delights the toper, the glass which souses him and puts the finishing touch on his drunkenness. Each pleasure reserves to the end the greatest delights which it contains. Life is most delightful when it is on the downward slope, but has not yet reached the abrupt decline.” 
-― Seneca, Letters from a Stoic
-
-“My fiftieth year had come and gone,
-I sat, a solitary man,
-In a crowded London shop,
-An open book and empty cup
-On the marble table-top.
-While on the shop and street I gazed
-My body of a sudden blazed;
-And twenty minutes more or less
-It seemed, so great my happiness,
-That I was blessed and could bless.” 
-― W.B. Yeats, The Collected Poems of W.B. Yeats
-
-“[MAGISTRATE]
-Don't men grow old?
-[LYSISTRATA]
-Not like women. When a man comes home
-Though he's grey as grief he can always get a girl.
-There's no second spring for a woman. None.
-She can't recall it, nobody wants her, however
-She squanders her time on the promise of oracles,
-It's no use...” 
-― Aristophanes, Lysistrata
-
-“The compensation of growing old...was simply this; that the passions remain as strong as ever, but one has gained - at last! - the power which adds the supreme flavor to existence, - the power of taking hold of experience, of turning it round, slowly, in the light.” 
-― Virginia Woolf
-
-“We must stand up against old age and make up for its drawbacks by taking pains. We must fight it as we should an illness. We must look after our health, use moderate exercise, take just enough food and drink to recruit, but not to overload, our strength. Nor is it the body alone that must be supported, but the intellect and soul much more.” 
-― Marcus Tullius Cicero, Treatises on Friendship and Old Age
-
-“My aunt must have been perfectly well aware that she would not see Swann again, that she would never leave her own house any more, but this ultimate seclusion seemed to be accepted by her with all the more readiness for the very reason which, to our minds, ought to have made it more unbearable; namely, that such a seclusion was forced upon her by the gradual and steady diminution in her strength which she was able to measure daily, which, by making every action, every movement 'tiring' to her if not actually painful, gave to inaction, isolation and silence the blessed, strengthening and refreshing charm of repose.” 
-― Marcel Proust, Swann's Way
-
-“Forty-two. His age had astounded him for years, and each time that he had sat so astounded, trying to figure out what had become of the young, slim man in his twenties, a whole additional year slipped by and had to be recorded, a continually growing sum which he could not reconcile with his self-image. He still saw himself, in his mind's eye, as youthful, and when he caught sight of himself in photographs he usually collapsed ... Somebody took my actual physical presence away and substituted this, he had thought from time to time. Oh well, so it went.” 
-― Philip K. Dick, A Maze of Death
-
-“So you must not think a man has lived long because he has white hair and wrinkles: he has not lived long, just existed long. For suppose you should think that a man had had a long voyage who had been caught in a raging storm as he left harbour, and carried hither and thither and driven round and round in a circle by the rage opposing winds. He did not have a long voyage, just a long tossing about.” 
-― Seneca, On the Shortness of Life
-
-“I don't see why there should be a point where everyone decides you're too old. I'm not too old, and until I decide I'm too old I'll never be too fucking old.” 
-― Lemmy Kilmister
-
-“..Thirty's the end of being young, forty is where you stop fooling yourself.” 
-― Stephen King, The Bachman Books
-
-
-“Some day you will be old enough to start reading fairy tales again.” 
-― C.S. Lewis
-
-“I am not young enough to know everything.” 
-― Oscar Wilde
-
-“Wrinkles should merely indicate where the smiles have been.” 
-― Mark Twain
-
-“Age does not make us childish, as some say; it finds us true children.” 
-― Johann Wolfgang von Goethe
-
-“By the time you read this, you'll be older than you remember.” 
-― Chuck Palahniuk, Rant
-
-“One by one they were all becoming shades. Better pass boldly into that other world, in the full glory of some passion, than fade and wither dismally with age.” 
-― James Joyce, Dubliners
-
-“Is someone different at age 18 or 60? I believe one stays the same.” 
-― Hayao Miyazaki
-
-“By the time you're thirty, your worst enemy is yourself.” 
-― Chuck Palahniuk, Choke
-
 “My child, I know you're not a child
 But I still see you running wild
 Between those flowering trees.
@@ -167,78 +61,23 @@ Or turned into gold.
 This is the age you are broken or turned into gold.” 
 ― Antonia Michaelis, The Storyteller
 
-“Age is an issue of mind over matter. If you don’t mind, it doesn’t matter.” 
-― Mark Twain
-
-“Indeed, no woman should ever be quite accurate about her age. It looks so calculating.” 
-― Oscar Wilde, The Importance of Being Earnest
-
-“With age comes wisdom, but sometimes age comes alone.” 
-― Oscar Wilde
-
-“Nemo enim est tam senex qui se annum non putet posse vivere.
-
-(No one is so old as to think that he cannot live one more year.)” 
-― Marcus Tullius Cicero, On Old Age, On Friendship & On Divination
-
-“In my dreams, I never have an age.” 
-― Madeleine L'Engle
-
-“Old men tend to forget what thought was like in their youth; they forget the quickness of the mental jump, the daring of the youthful intuition, the agility of the fresh insight. They become accustomed to the more plodding varieties of reason, and because this is more than made up by the accumulation of experience, old men think themselves wiser than the young.” 
-― Isaac Asimov, Pebble in the Sky
-
-“Those of us who are blamed when old for reading childish books were blamed when children for reading books too old for us.” 
-― C.S. Lewis
-
-“A man that is young in years may be old in hours if he have lost no time. ” 
-― Francis Bacon
-
-“[The Coming of Wisdom with Time]
-Though leaves are many, the root is one;
-Through all the lying days of my youth
-I swayed my leaves and flowers in the sun;
-Now I may wither into the truth.” 
-― W.B. Yeats
-
-“Whatever this is that I am, it is a little flesh and breath, and the ruling part. Throw away thy books; no longer distract thyself: it is not allowed; but as if thou wast now dying, despise the flesh; it is blood and bones and a network, a contexture of nerves, veins, and arteries. See the breath also, what kind of a thing it is, air, and not always the same, but every moment sent out and again sucked in. The third then is the ruling part: consider thus: Thou art an old man; no longer let this be a slave, no longer be pulled by the strings like a puppet to unsocial movements, no longer be either dissatisfied with thy present lot, or shrink from the future.” 
-― Marcus Aurelius, Meditations
-
-“Age is a seasoned trickster. To our parents, we will always be children. Within ourselves, the same yearnings of youth; the same aspirations of adolescence, will last a lifetime. Only to the young - blinded by our grey hair and slowing gait - do we appear old and increasingly beyond the pale.” 
-― Alex Morritt, Impromptu Scribe
-
-“You see, I don't think age matters so much as people think. Parts of me are still 12 and I think other parts were already 50 when I was 12….” 
-― C.S. Lewis, Letters to Children
-
-“You don't know yet what money is. Money is power, when you have lived as long as I have. I know, I know. If youth but knew. But what does Shakespeare say? Put money in thy purse.” 
-― James Joyce, Ulysses
-
-“You're enough to try the patience of an oyster!” 
-― Lewis Carroll, Alice's Adventures in Wonderland & Through the Looking-Glass
-
-“There is nothing which for my part I like better, Cephalus, than conversing with aged men; for I regard them as travellers who have gone a journey which I too may have to go, and of whom I ought to inquire, whether the way is smooth and easy, or rugged and difficult.” 
-― Plato, The Republic
-
-“That's what is was to be young — to be enthusiastic rather than envious about the good work other people could do.” 
-― Kurt Vonnegut, Palm Sunday: An Autobiographical Collage
-
-“His age was indeterminate. But in cynicism and general world weariness, which is a sort of carbon dating of the personality, he was about seven thousand years old.” 
-― Terry Pratchett
-
-“The longer I live the more I see that I am never wrong about anything, and that all the pains I have so humbly taken to verify my notions have only wasted my time.” 
-― George Bernard Shaw
-
-“Rather I think that a man who ... is willing ... to value learning as long as he lives, not supposing that old age brings him wisdom of itself, will necessarily pay more attention to the rest of his life.” 
-― Plato, Laches
-
-“When she looked in the glass and saw her hair grey her cheek sunk, at fifty, she thought, possibly she might have managed things better--her husband; money; his books. But for her own part she would never for a single second regret her decision, evade difficulties, or slur over duties” 
-― Virginia Woolf, To the Lighthouse
-
-“People are afraid of themselves, nowadays. They have forgotten the highest of all duties, the duty that one owes to one's self. Of course, they are charitable. They feed the hungry and clothe the beggar. But their own souls starve, and are naked. Courage has gone out of our race. Perhaps we never really had it. The terror of society, which is the basis of morals, the terror of God, which is the secret of religion—these are the two things that govern us. And yet—” 
-― Oscar Wilde, The Picture of Dorian Gray
-
 
 
 
 
 http://www.shakespeare-online.com/sonnets/60.html
-
+Like as the waves make towards the pebbled shore,
+So do our minutes hasten to their end; 
+Each changing place with that which goes before,
+In sequent toil all forwards do contend. 
+Nativity, once in the main of light, 
+Crawls to maturity, wherewith being crown'd,
+Crooked eclipses 'gainst his glory fight, 
+And Time, that gave, doth now his gift confound.
+Time doth transfix the flourish set on youth, 
+And delves the parallels in beauty's brow, 
+Feeds on the rarities of nature's truth, 
+And nothing stands but for his scythe to mow.
+   And yet to times in hope my verse shall stand,
+   Praising thy worth, despite his cruel hand.
+   
