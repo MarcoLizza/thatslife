b54799a0e46b5aa081ff51f199070d9c06c12f7a
Marco Lizza <marco.lizza@gmail.com>
2016-05-10 17:51:30 +0200

    Adding timer module.

---
+++ lib/timer.lua
@@ -0,0 +1,49 @@
+--[[
+
+Copyright (c) 2016 by Marco Lizza (marco.lizza@gmail.com)
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgement in the product documentation would be
+   appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
+
+]]--
+
+-- MODULE DECLARATION ----------------------------------------------------------
+
+local Timer = {
+  _VERSION = '0.1.0'
+}
+
+-- MODULE OBJECT CONSTRUCTOR ---------------------------------------------------
+
+-- MODULE FUNCTIONS ------------------------------------------------------------
+
+function Timer.create(period, callback)
+  local accumulator = 0
+  local closure = function(dt)
+        accumulator = accumulator + dt
+        if accumulator >= period then
+          callback()
+          accumulator = 0
+        end
+      end
+  return closure
+end
+
+-- END OF MODULE ---------------------------------------------------------------
+
+return Timer
+
+-- END OF FILE -----------------------------------------------------------------
