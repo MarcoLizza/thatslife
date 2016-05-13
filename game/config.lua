575c3ece9d86a1982d5c9edb0cfa10dc89c92712
Marco Lizza <marco.lizza@gmail.com>
2016-05-13 23:46:14 +0200

    Purging configuration.

--- game/config.lua
+++ game/config.lua
@@ -7,26 +7,7 @@ return {
     ratio = 0.95,
     scale = -1
   },
-  starfield = {
-    layers = 4,
-    min_speed = 5,
-    max_speed = 25,
-    stars_per_layer = 32,
-    reorient_timeout = 30
+  game = {
+    duration = 60
   }
 }
---sparkle = {
---radius=1
---min_speed=64
---max_speed=128
---min_life=1
---max_life=3
---}
---explosion = {
---min_radius=3
---max_radius=6
---min_speed=64
---max_speed=128
---min_life=1
---max_life=3
---}
